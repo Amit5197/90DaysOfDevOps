5cc469fed2fcf34d8964e982991063a15e64a3fb
Afroz Shaikh <afsk1997@gmail.com>
2026-02-28 03:34:15 +0530

    Update strict_demo.sh

--- 2026/day-18/scripts/strict_demo.sh
+++ 2026/day-18/scripts/strict_demo.sh
@@ -6,7 +6,7 @@ echo "Check set -o pipefail"
 cat count.txt | grep "total"
 echo "After failing script running without set -o"
 
-echo -e "\n"
+echo -u "\n"
 echo "Undefined variable -u"
 echo $a
 echo "After using undefined variable script running without set -u"
