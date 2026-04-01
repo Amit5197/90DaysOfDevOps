ab9e3076037e1478b366d8be15a2e37480c78af0
srdangat <sanket.r.dangat@gmail.com>
2026-04-01 21:45:43 +0530

    Complete Day 64 Tasks

---
+++ 2026/day-64/awsinfra-state-mgmt/variables.tf
@@ -0,0 +1,53 @@
+variable "region" {
+  description = "AWS region where resources will be provisioned"
+  type        = string
+  default     = "us-east-1"
+}
+
+variable "vpc_cidr" {
+  description = "CIDR block for the VPC"
+  type        = string
+  default     = "10.0.0.0/16"
+}
+
+variable "subnet_cidr" {
+  description = "CIDR block for the subnet"
+  type        = string
+  default     = "10.0.1.0/24"
+}
+
+variable "instance_type" {
+  description = "EC2 instance type"
+  type        = string
+  default     = "t3.micro"
+}
+
+variable "key_name" {
+  description = "SSH key pair name"
+  type        = string
+  default     = "josh-batch"
+}
+
+variable "project_name" {
+  description = "Name of the project"
+  type        = string
+  default     = "Terraweek"
+}
+
+variable "environment" {
+  description = "Deployment environment (e.g. dev,staging,prod)"
+  type        = string
+  default     = "Dev"
+}
+
+variable "allowed_ports" {
+  description = "List of allowed inbound ports"
+  type        = list(number)
+  default     = [22, 80, 443]
+}
+
+variable "extra_tags" {
+  description = "Additional tags for resources"
+  type        = map(string)
+  default     = {}
+}
