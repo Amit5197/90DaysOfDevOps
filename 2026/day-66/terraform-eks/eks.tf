ec66adfb1caf85a704d15f2c780166cec1587006
Afroz-J-Shaikh <afsk1997@gmail.com>
2026-04-05 07:57:36 +0530

    day66 done

---
+++ 2026/day-66/terraform-eks/eks.tf
@@ -0,0 +1,45 @@
+data "aws_caller_identity" "current" {}
+module "eks" {
+  source = "terraform-aws-modules/eks/aws"
+  version = "~> 20.0"
+  cluster_name    = var.cluster_name
+  cluster_version = var.cluster_version
+
+  vpc_id     = module.vpc.vpc_id
+  subnet_ids = module.vpc.private_subnets
+
+  cluster_endpoint_public_access = true
+  cluster_endpoint_private_access = true  # ← MUST be true for private subnets
+
+  eks_managed_node_groups = {
+    terraweek_nodes = {
+      ami_type       = "AL2_x86_64"
+      instance_types = [var.node_instance_type]
+
+      min_size     = 1
+      max_size     = 3
+      desired_size = var.node_desired_count
+    }
+  }
+
+  access_entries = {
+    current_user = {
+      principal_arn = data.aws_caller_identity.current.arn
+
+      policy_associations = {
+        admin = {
+          policy_arn = "arn:aws:eks::aws:cluster-access-policy/AmazonEKSClusterAdminPolicy"
+          access_scope = {
+            type = "cluster"
+          }
+        }
+      }
+    }
+  }
+  
+  tags = {
+    Environment = "dev"
+    Project     = "TerraWeek"
+    ManagedBy   = "Terraform"
+  }
+}
