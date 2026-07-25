bf095411d949fd93fd030b63dd7562a62166a4fe
Amit Kumar Pandey <bhai.amit7@gmail.com>
2026-07-25 10:03:03 +0530

    Create variables.tf

---
+++ 2026/day-63/terraform-aws-infra-refactor/variables.tf
@@ -0,0 +1,52 @@
+variable "region" {
+  description = "AWS region where resources will be provisioned"
+  type        = string
+  default     = "us-east-1"
+}
+
+variable "vpc_cidr" {
+  description = "CIDR block for the VPC"
+  type        = string
+  default     = "10.0.0.0/16"
+}
+
+variable "subnet_cidr" {
+  description = "CIDR block for the subnet"
+  type        = string
+  default     = "10.0.1.0/24"
+}
+
+variable "instance_type" {
+  description = "EC2 instance type"
+  type        = string
+  default     = "t2.micro"
+}
+
+variable "key_name" {
+  description = "SSH key pair name"
+  type        = string
+  default     = "terra-key"
+}
+
+variable "project_name" {
+  description = "Name of the project"
+  type        = string
+}
+
+variable "environment" {
+  description = "Deployment environment (e.g. dev,staging,prod)"
+  type        = string
+  default     = "dev"
+}
+
+variable "allowed_ports" {
+  description = "List of allowed inbound ports"
+  type        = list(number)
+  default     = [22, 80, 443]
+}
+
+variable "extra_tags" {
+  description = "Additional tags for resources"
+  type        = map(string)
+  default     = {}
+}
