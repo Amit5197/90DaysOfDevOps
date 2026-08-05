c3536e236446f60d1ee8eea6e42af5d8097e59c3
Amit Kumar Pandey <bhai.amit7@gmail.com>
2026-08-05 11:46:28 +0530

    Update main.tf

--- 2026/day-67/terraweek-capstone/main.tf
+++ 2026/day-67/terraweek-capstone/main.tf
@@ -25,12 +25,10 @@ data "aws_ami" "amazon_linux_2" {
   }
 }
 
-
-
 module "vpc" {
   source             = "./modules/vpc"
-  cidr               = var.vpc_cidr
-  public_subnet_cidr = var.subnet_cidr
+  cidr               = local.vpc_cidr
+  public_subnet_cidr = local.subnet_cidr
   environment        = local.environment
   project_name       = var.project_name
 }
@@ -38,7 +36,7 @@ module "vpc" {
 module "security_group" {
   source        = "./modules/security-group"
   vpc_id        = module.vpc.vpc_id
-  ingress_ports = var.ingress_ports
+  ingress_ports = local.ingress_ports
   environment   = local.environment
   project_name  = var.project_name
 }
@@ -46,7 +44,7 @@ module "security_group" {
 module "ec2" {
   source             = "./modules/ec2-instance"
   ami_id             = data.aws_ami.amazon_linux_2.id
-  instance_type      = var.instance_type
+  instance_type      = local.instance_type
   subnet_id          = module.vpc.subnet_id
   security_group_ids = [module.security_group.sg_id]
   environment        = local.environment
